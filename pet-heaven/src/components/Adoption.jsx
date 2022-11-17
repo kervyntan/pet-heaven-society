a46de92ffc09d18f83dca3742ff0b27a3aec18e4
iamkervyntan <kervyntan@gmail.com>
2022-11-17 23:11:21 +0800

    Added function to loop through array of dogs and display

--- pet-heaven/src/components/Adoption.jsx
+++ pet-heaven/src/components/Adoption.jsx
@@ -5,11 +5,13 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import AdoptForm from './AdoptForm';
+import DogPic from './shared/DogPic';
 // need to fetch the data
 
 const Adoption = () => {
     let counter = 10;
-    const [dogs, setDogs] = useState('')
+    // need to initialise as an array at the start
+    const [dogs, setDogs] = useState([])
     const [loading, setLoading] = useState(true);
 useEffect( () => {
     // while(counter !== 0) {
@@ -19,23 +21,29 @@ useEffect( () => {
     //     // setDogs({...dogs, "url" : data.message})
     //     counter--;
     // }
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch("https://dog.ceo/api/breeds/image/random/10")
     .then( (res) => res.json() )
     .then( (data) => setDogs(data.message));
     setLoading(false);
 }, [])
+
+const items = dogs.map((element) => {
+    return (
+        <>
+            <DogPic src={element} />
+        </>
+    )
+});
+console.log(items);
 
 
     return (
         <div className="container">
             <h2> Pets available for Adoption! </h2>
             <div className="adopt">
-            {!loading &&
-            <>
-                <img width="300" src={dogs} />
-                <h2>Tracy</h2>
-            </>
-            }
+            {/* {!loading && */}
+                {/* <img width="300" src={dogs} /> */}
+                {items}
             </div>
             <AdoptForm />
         </div>
---
+++ pet-heaven/src/components/shared/DogPic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DogPic = (props) => {
+    return (
+        <>
+        <img width="300" height="300" src={props.src} />
+        </>
+    )
+}
+
+export default DogPic;
